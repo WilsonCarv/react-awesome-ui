108a237ba5e7244e004a55c3bba708126384be70
Wilson Carvajal <wilsoncarvajal@Wilsons-MacBook-Pro.local>
2024-03-26 15:42:54 -0600

    testing pimary buttons

--- src/stories/ButtonComponent.stories.js
+++ src/stories/ButtonComponent.stories.js
@@ -1,9 +1,18 @@
+import { useRef } from 'react';
 import { ButtonComponent } from '../components/Button/ButtonComponent';
 
 export default {
 	title: 'ButtonComponent',
 	component: ButtonComponent
 };
+const handleClick = () => {};
+const buttonRef = useRef(null);
 export const Primary = {
-	args: { text: 'Primary Button' }
+	args: {
+		text: 'Primary Button',
+		type: 'primary',
+		size: 'regular',
+		onClick: handleClick,
+		ref: buttonRef
+	}
 };
