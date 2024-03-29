22a889a04348ba5fffb81e0decb80ee6d337db92
Wilson Carvajal <wilsoncarvajal@Wilsons-MacBook-Pro.local>
2024-03-28 19:15:34 -0600

    install pack

--- src/components/Autocomplete/AutoCompleteComponent.jsx
+++ src/components/Autocomplete/AutoCompleteComponent.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useState, useRef, useEffect } from 'react';
+import React, { Fragment, useState } from 'react';
 import PropTypes from 'prop-types';
 import './AutoCompleteComponent.css';
 
